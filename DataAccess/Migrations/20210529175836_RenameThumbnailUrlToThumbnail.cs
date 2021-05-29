0b446f37cf2094f0e5d5302b7ef46e0544ec1fa8
Alex Boicu <alex.c.boicu@gmail.com>
2021-05-29 20:59:17 +0300

    Add thumnail renaming migration

---
+++ DataAccess/Migrations/20210529175836_RenameThumbnailUrlToThumbnail.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Crowdly_BE.Migrations
+{
+    public partial class RenameThumbnailUrlToThumbnail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ThumbnailUrl",
+                table: "Vendors",
+                newName: "Thumbnail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Thumbnail",
+                table: "Vendors",
+                newName: "ThumbnailUrl");
+        }
+    }
+}
