69b122c40e2aac5ec1bc8a35fbb716bb8fee89b2
Alex Boicu <alex.c.boicu@gmail.com>
2021-05-29 20:36:59 +0300

    Add vendor extra fields

--- Crowdly-BE/Vendors/Vendor.cs
+++ Crowdly-BE/Vendors/Vendor.cs
@@ -25,6 +25,11 @@ namespace Crowdly_BE.Vendors
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
         public string[] ImageUrls { get; set; }
         public bool IsEditable { get; set; }
--- DataAccess/Models/Vendor.cs
+++ DataAccess/Models/Vendor.cs
@@ -7,6 +7,11 @@ namespace DataAccess.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
         public string ThumbnailUrl { get; set; }
         public string[] ImageUrls { get; set; }
